19d42999ae16aa45811e8bc48d3f26834512c647
will <will@256k1.dev>
2025-09-04 22:43:33 +0100

    add derivation

--- flake.nix
+++ flake.nix
@@ -27,10 +27,36 @@
         with pkgs; {
           formatter = alejandra;
 
-          packages.default = callPackage ./. {
+          packages.default = let
             rustPlatform = makeRustPlatform {
               cargo = rust-bin.stable.latest.default;
               rustc = rust-bin.stable.latest.default;
+            };
+          in rustPlatform.buildRustPackage rec {
+            pname = "fetch-tasks-github";
+            version = "0.1.0";
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = [
+              pkg-config
+            ];
+
+            buildInputs = [
+              openssl
+            ];
+
+            # Skip tests during build as they may require network access
+            doCheck = false;
+
+            meta = with lib; {
+              description = "Bitcoin Core CI fetcher for GitHub Actions data";
+              license = licenses.mit;
+              maintainers = [ ];
             };
           };
 

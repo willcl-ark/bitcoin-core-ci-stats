38927cf1ace318d314aa6bdcb23b37b8d6b71742
will <will@256k1.dev>
2026-02-02 08:52:20 +0000

    use crane in flake

--- flake.nix
+++ flake.nix
@@ -2,71 +2,52 @@
   description = "bitcoin core ci fetcher";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {inherit system overlays;};
-      in
-        with pkgs; {
-          formatter = alejandra;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource ./.;
 
-          packages.default = let
-            rustPlatform = makeRustPlatform {
-              cargo = rust-bin.stable.latest.default;
-              rustc = rust-bin.stable.latest.default;
-            };
-          in rustPlatform.buildRustPackage rec {
-            pname = "fetch-tasks-github";
-            version = "0.1.0";
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+        };
 
-            src = ./.;
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-
-            nativeBuildInputs = [
-              pkg-config
-            ];
-
-            buildInputs = [
-              openssl
-            ];
-
-            # Skip tests during build as they may require network access
+        fetch-tasks-github = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
             doCheck = false;
+          }
+        );
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
 
-            meta = with lib; {
-              description = "Bitcoin Core CI fetcher for GitHub Actions data";
-              license = licenses.mit;
-              maintainers = [ ];
-            };
-          };
+        packages.default = fetch-tasks-github;
 
-          devShells.default = mkShell {
-            stdenv = gcc14Stdenv;
-            nativeBuildInputs = [];
-            buildInputs = [
-              rust-bin.stable.latest.default
-            ];
-          };
-        }
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system} or { };
+          packages = [ ];
+        };
+      }
     );
 }
